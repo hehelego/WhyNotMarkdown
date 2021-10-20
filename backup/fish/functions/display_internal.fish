a2db5d22d50b658528d185a6b6784b3bf5942dd4
hehelego <2364261262@qq.com>
2021-10-20 21:02:23 +0800

    upd(backup): rename fish functions, remove ambiguous menu entries

--- backup/fish/functions/display_internal.fish
+++ backup/fish/functions/display_internal.fish
@@ -1,4 +1,4 @@
-function display_default
+function display_internal
 	xrandr --output eDP --mode 2560x1600 --rate 60 --gamma 1:1:1 --dpi 192 --set TearFree on --orientation normal
 	xrandr --output DisplayPort-0 --off
 	xrandr --output DisplayPort-1 --off
