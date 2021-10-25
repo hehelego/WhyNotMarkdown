45ae75616f390b6f8db2951ef584841f0af0f126
hehelego <2364261262@qq.com>
2021-10-25 09:36:09 +0800

    upd(backup): starship
    up

---
+++ backup/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,4 @@
+function fish_right_prompt
+        starship prompt --right --status=$STARSHIP_CMD_STATUS --pipestatus=$STARSHIP_CMD_PIPESTATUS --keymap=$STARSHIP_KEYMAP --cmd-duration=$STARSHIP_DURATION --jobs=$STARSHIP_JOBS
+    
+end
