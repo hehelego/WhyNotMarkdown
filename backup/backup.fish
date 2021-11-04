b16df9be2c83f15a665a1de3d9cf5b1531071e89
hehelego <2364261262@qq.com>
2021-11-04 12:07:10 +0800

    upd(backup): add sway config, update dotfiles backup script

--- backup/backup.fish
+++ backup/backup.fish
@@ -1,3 +1,4 @@
+#!/usr/bin/fish
 # wrapper around rsync
 function backup
 	if test (count $argv) -ge 2
@@ -22,23 +23,27 @@ backup_setup
 
 
 
+
 backup ~/.ssh/config                                 ~/.workspace/git-repo/WhyNotMarkdown/backup/ssh.conf
 backup ~/.tmux.conf                                  ~/.workspace/git-repo/WhyNotMarkdown/backup/tmux.conf
 backup ~/.config/systemd/                            ~/.workspace/git-repo/WhyNotMarkdown/backup/systemd/
 backup ~/.config/environment.d/                      ~/.workspace/git-repo/WhyNotMarkdown/backup/environment.d/
 backup ~/.config/i3/                                 ~/.workspace/git-repo/WhyNotMarkdown/backup/i3_conf/i3_conf/
+backup ~/.config/sway/                               ~/.workspace/git-repo/WhyNotMarkdown/backup/i3_conf/sway/
 backup ~/.config/i3status-rust/                      ~/.workspace/git-repo/WhyNotMarkdown/backup/i3_conf/i3status-rust/
+backup ~/.config/dunst/                              ~/.workspace/git-repo/WhyNotMarkdown/backup/i3_conf/dunst/
 backup ~/.config/fish/                               ~/.workspace/git-repo/WhyNotMarkdown/backup/fish/
 backup ~/.config/starship/                           ~/.workspace/git-repo/WhyNotMarkdown/backup/starship/
 backup ~/.config/nvim/                               ~/.workspace/git-repo/WhyNotMarkdown/backup/nvim/
 backup ~/.config/alacritty/                          ~/.workspace/git-repo/WhyNotMarkdown/backup/alacritty/
-backup ~/.config/dunst/                              ~/.workspace/git-repo/WhyNotMarkdown/backup/i3_conf/dunst/
 backup ~/.config/rofi/                               ~/.workspace/git-repo/WhyNotMarkdown/backup/rofi/
 backup ~/.config/keynav/                             ~/.workspace/git-repo/WhyNotMarkdown/backup/keynav/
 backup ~/.config/qutebrowser/                        ~/.workspace/git-repo/WhyNotMarkdown/backup/qutebrowser/
 backup ~/.config/fontconfig/fonts.conf               ~/.workspace/git-repo/WhyNotMarkdown/backup/font.conf
 backup ~/.workspace/firefox-bookmarks.json           ~/.workspace/git-repo/WhyNotMarkdown/backup/FF_bookmarks.json
 backup ~/.workspace/backup.fish                      ~/.workspace/git-repo/WhyNotMarkdown/backup/backup.fish
+
+
 
 backup ~/.workspace/Passwords.kdbx                   ~/.workspace/BACKUP/Passwords.kdbx
 backup ~/.workspace/thunderbird-calendar.ics         ~/.workspace/BACKUP/thunderbird-calendar.ics
